920096d0b1be9d5063295b03c9ac5d19af90c14a
Vraj1103 <svraj073@gmail.com>
2023-03-30 19:41:44 +0530

    gained the knowledge about fs read and write

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-const name = "Vraj";
-console.log(name);
+// const name = "Vraj";
+// console.log(name);
+
+// const fs = require("fs");
+//  fs.writeFileSync("read.txt", "Welcome to Node.js");
+//  fs.appendFileSync("read.txt", " This is a new line");
+
+// const buf_data = fs.readFileSync("read.txt");
+// console.log(buf_data);
+// org_data = buf_data.toString();
+// console.log(org_data);
+
+// fs.renameSync("read.txt", "readwrite.txt");
