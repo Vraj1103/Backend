023fb88e7f77b4d8e9173f460931736026ba705f
Vraj1103 <svraj073@gmail.com>
2023-04-03 00:07:37 +0530

    JSON conversion and transfer of data

--- JsonNodeJs/index.js
+++ JsonNodeJs/index.js
@@ -3,12 +3,23 @@
 // Object to JSON -> Stringify
 // JSON to object -> Parse
 
-const bioData ={
-    name : "Vraj",
-    age : 20,
-    company_name : "Curiosity Science",
-};
+// const fs = require("fs");
+// const bioData ={
+//     name : "Vraj",
+//     age : 20,
+//     company_name : "Curiosity Science",
+// };
 
 // convert it to JSON
-const jsonData = JSON.stringify(bioData);
+// const jsonData = JSON.stringify(bioData);
+
+// send the data in the form of json to other file
+// fs.writeFile("josn1.json",jsonData,(err)=>{
+//     console.log(err);
+// })
+
+// convert json to object and read that from other file
+// fs.readFile("josn1.json","utf-8",(err,data)=>{
+//     console.log(JSON.parse(data));
+// })
 
